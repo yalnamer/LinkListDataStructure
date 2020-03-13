0652f11f38aaf9cacebfafef19546568c36a1191
yalnamer <yyousef.alnamer@gmail.com>
2020-03-13 01:08:04 -0400

    Added comments, and created ability to insert to end

--- LinkedListDataStructure/LinkedList.h
+++ LinkedListDataStructure/LinkedList.h
@@ -6,8 +6,7 @@ template <class T> class LinkedList
 
 private:
 
-	Node<T>* head;
-	
+	Node<T>* head; //> Store the pointer to the first element
 
 public:
 
@@ -15,8 +14,10 @@ public:
 
 	LinkedList();
 
-	void insert(T x);
-	void insert(T x, int position);
+	//Insert a item into the linked list, by default items are inserted to beginning
+	void insert(T x, bool insertBeg = true);
+	// Insert a element into the nth position
+	void insert(T x, int position); 
 
 	void print();
 };
@@ -29,28 +30,53 @@ inline LinkedList<T>::LinkedList()
 }
 
 template<class T>
-inline void LinkedList<T>::insert(T x)
+inline void LinkedList<T>::insert(T x,bool insertBeg)
 {
-	Node<T>* temp = new Node<T>();//Create a new node
-	temp->data = x; //> set data in new node to x
-	temp->next = head;
-	head = temp;
+	Node<T>* temp1 = new Node<T>();//Create a new node
+	temp1->data = x; //> set data in new node to x
+	temp1->next = head; //>new node now points to where head points
+
+	
+	if (insertBeg || length == 0) //> insert to beginning, if insertBeg setting on, or empty list
+	{
+		head = temp1; //> head points to new node
+	}
+	else
+	{
+		Node<T>* temp2 = head; // create a temp node for traversal
+
+		//traverse to element before last element
+		for (int i = 0; i < length - 1; i++)
+			temp2 = temp2->next;
+		
+		
+		//Fix links
+		temp1->next = nullptr; //new to nullptr, which indicates end
+		temp2->next = temp1; //element at last node now point
+
+	}
 	length++;
 }
 
 template<class T>
 inline void LinkedList<T>::insert(T x, int position)
 {
+	//Make sure that position is valid for linked list
 	if (position < 0 || position >(length + 1))
 	{
 		std::cout << "Error: Out of Range\n";
 		return;
 	}
 
+	//Create a new node to add to linked list
 	Node<T>* temp1 = new Node<T>();
-	temp1->data = x;
-	temp1->next = head;
+	temp1->data = x; //> Store data into new element
+	temp1->next = head; //>Temporarily set address to head, to start traversing
 
+	// in the case that the desired position is the 0th element( the first element)
+	// we don't need to traverse, all we have to do is adjust the links of the nodes
+	// the new element now points to the element that the head used to point to
+	// and the head now points to the new element itself, finished
 	if (position == 0)
 	{
 		temp1->next = head;
@@ -59,15 +85,14 @@ inline void LinkedList<T>::insert(T x, int position)
 		return;
 	}
 
-	Node<T>* temp2 = head;
+	Node<T>* temp2 = head; //Create a node to use for traversal
 
+	//traverse to the element before the desired position
 	for (int i = 0; i < (position - 1);i++)
-	{
 		temp2 = temp2->next;
-	}
 
-	temp1->next = temp2->next;
-	temp2->next = temp1;
+	temp1->next = temp2->next;//>the new node now points to the element which was in the nth position
+	temp2->next = temp1; //> the the element in the n-1 position now points to the new node
 
 	length++;
 }
@@ -75,6 +100,7 @@ inline void LinkedList<T>::insert(T x, int position)
 template<class T>
 inline void LinkedList<T>::print()
 {
+	
 	Node<T>* temp = head;
 	while (temp != nullptr)
 	{
--- LinkedListDataStructure/main.cpp
+++ LinkedListDataStructure/main.cpp
@@ -13,40 +13,49 @@ int main()
 	LinkedList<int> list;
 
 	
-	//int n; //How many numbers does user want to input?
+	//CODE TO TEST LINKED LIST WITH USER INPUT
+	////int n; //How many numbers does user want to input?
 
-	//
-	//while (std::cout << "How many numbers would you like to input: " && !(std::cin >> n))
-	//{
-	//	std::cout << "Not a Valid Number \n";
-	//	std::cin.clear();
-	//	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	//}
-	//
+	////
+	////while (std::cout << "How many numbers would you like to input: " && !(std::cin >> n))
+	////{
+	////	std::cout << "Not a Valid Number \n";
+	////	std::cin.clear();
+	////	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	////}
+	////
 
-	//std::cout << "\n";
+	////std::cout << "\n";
 
-	//for (int i = 0; i < n; i++)
-	//{
-	//	int x;
+	////for (int i = 0; i < n; i++)
+	////{
+	////	int x;
 
-	//	bool valid = false;
+	////	bool valid = false;
 
-	//	while (std::cout << "Enter your number: " && !(std::cin >> x))
-	//	{
-	//		std::cout << "!! Not a Valid Number\n";
-	//		std::cin.clear();
-	//		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	//	}
-	//	list.insert(x);
-	//	list.print();
+	////	while (std::cout << "Enter your number: " && !(std::cin >> x))
+	////	{
+	////		std::cout << "!! Not a Valid Number\n";
+	////		std::cin.clear();
+	////		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	////	}
+	////	list.insert(x);
+	////	list.print();
 
-	//}
+	////}
+	//END OF TEST CODE
 
-	list.insert(878, 0);
+	//list.insert(5);
+	list.insert(21, false);
+	list.insert(14);
+	list.insert(5, false);
+	list.insert(23, 0);
+	/*list.insert(878, 0);
 	list.insert(5);
 	list.insert(23, 1);
 	list.insert(13);
-
+	list.insert(12, 23);
+	list.insert(14, false);
+*/
 	list.print();
 }
